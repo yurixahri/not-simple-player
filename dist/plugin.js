e665d1b86d124abaf6343bf725df00c63d054cb3
thinhthui <111081318+thinhthui@users.noreply.github.com>
2023-03-14 22:20:28 +0700

    add jsmediatags

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,4 +1,4 @@
-exports.version = 1.1
+exports.version = 1.2
 exports.description = "Not simple player - add the custom.html in the repo before using"
 exports.repo = "thinhthui/not-simple-player"
 exports.apiRequired = 5
