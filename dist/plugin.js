5a5713eeb4e9735f2961790a20fa687bc3be1393
thinhthui <111081318+thinhthui@users.noreply.github.com>
2023-05-07 22:25:56 +0700

    old version for HFS < 0.45

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,17 +1,6 @@
-exports.version = 1.3
+exports.version = 1.2
 exports.description = "Not simple player - add the custom.html in the repo before using"
 exports.repo = "thinhthui/not-simple-player"
-exports.apiRequired = 8.1
-exports.frontend_js = [
-    'main.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/mediaelement/4.2.7/mediaelement-and-player.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/jsmediatags/3.9.5/jsmediatags.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.0/FileSaver.min.js',
-]
-exports.frontend_css = [
-    'style.css',
-    "https://cdnjs.cloudflare.com/ajax/libs/mediaelement/4.2.7/mediaelementplayer.min.css",
-    "https://cdnjs.cloudflare.com/ajax/libs/mediaelement/4.2.7/mediaelement-and-player.min.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css",
-]
+exports.apiRequired = 5
+exports.frontend_js = 'main.js'
+exports.frontend_css = 'style.css'
