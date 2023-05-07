733887b9c2ca81463eaaae65d4ee6b573ecf1a2d
thinhthui <111081318+thinhthui@users.noreply.github.com>
2023-05-07 22:28:11 +0700

    Update plugin.js

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,5 +1,5 @@
 exports.version = 1.2
-exports.description = "Not simple player - add the custom.html in the repo before using"
+exports.description = "Not simple player (old)- add the custom.html in the repo before using"
 exports.repo = "thinhthui/not-simple-player"
 exports.apiRequired = 5
 exports.frontend_js = 'main.js'
