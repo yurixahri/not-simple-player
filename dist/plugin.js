1c1fb76a4493896c4acf82cbf922f8dc9e856563
thinhthui <111081318+thinhthui@users.noreply.github.com>
2023-05-07 22:29:46 +0700

    Update plugin.js

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,6 +1,6 @@
 exports.version = 1.2
 exports.description = "Not simple player (old)- add the custom.html in the repo before using"
-exports.repo = "thinhthui/not-simple-player"
+exports.repo = "thinhthui/not-simple-player/tree/nsp-old"
 exports.apiRequired = 5
 exports.frontend_js = 'main.js'
 exports.frontend_css = 'style.css'
