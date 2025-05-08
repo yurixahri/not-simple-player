bab77dfeef131ed1bcb6d447c8b259089b4edb5a
ahrixthinh <111081318+thinhthui@users.noreply.github.com>
2025-05-08 21:23:01 +0700

    Update main.js

--- dist/public/main.js
+++ dist/public/main.js
@@ -1,7 +1,38 @@
-HFS.onEvent('afterEntryName', ({ entry }, { h }) =>
-    /\.(mp3|wav|aac|ogg|flac|m4a)$/.test(entry.n) &&
-        h('button',{ className: 'play-button', onClick: () => play(entry.n) }))
+HFS.onEvent('afterEntryName', ({ entry, h }) =>
+  /mp3|wav|aac|ogg|flac/.test(entry.ext) &&
+  HFS.h('button', { className: 'play-button', onClick: () => play(entry.n) }))
 
+HFS.onEvent('beforeHeader', () => `
+  <div class="contain" >
+            <div class="music-player">
+			    <div class="cover">
+                    <img src="" alt="">   
+          </div>
+				<div class="wrap-text">
+					<p id="aud-info">nothing has played yet</p>
+				</div>
+				<div class="btn">
+				    <button onclick="backward()" style="color: #aeaeae4d; background-color: transparent; border: none; display: block; padding: 0; position: absolute; left: 30%; top: 18.5%; z-index: 4;"><i class="fa fa-backward"></i></button>
+				    <button onclick="forward()" style="color: #aeaeae4d; background-color: transparent; border: none; display: block; padding: 0; position: absolute; left: 63%; top: 18.5%; z-index: 4;"><i class="fa fa-forward"></i></button>
+					<button id="auto-play" style="color: #aeaeae4d; background-color: transparent; border: none; display: block; padding: 0; position: absolute; left: 55%; bottom: 13.5%; z-index: 4;"><i class="fa fa-wheelchair-alt"></i></button>
+				</div>
+                <div class="lecteur">
+                    <audio class="fc-media" src="null" autoplay></audio>
+                </div>
+            </div>
+</div>
+
+<div class="music_list">
+	<div style="display: flex; flex-direction: row; justify-content: space-between; align-items: center; border-bottom: 1px solid var(--faint-contrast);">
+	  <span>nothing</span>
+	  <button id="hide"></button>
+	</div>
+	<div id="scroll">
+	  <ul id="list">
+	  </ul>
+	</div>
+  </div>
+`)
 
 function play(name2 = '') {
   const root = document.querySelector('.fc-media')
@@ -10,28 +41,28 @@ function play(name2 = '') {
   acc.splice(-1);
   acc = acc.join('.');
   audio.src = encodeURIComponent(name2);
-	elem = document.querySelectorAll('.file a');
-	last_dir = location.href;
+  elem = document.querySelectorAll('.file a');
+  last_dir = location.href;
   var folder = last_dir.split('/');
 
   var cover = ""
-  $(".file a").each((i,a) => {
+  $(".file a").each((i, a) => {
     var name = $(a).text();
     var ext = name.split(".")[name.split(".").length - 1].trim()
-    if (["jpg","png","jpeg","jfif"].includes(ext)){
+    if (["jpg", "png", "jpeg", "jfif"].includes(ext)) {
       cover = $(a).attr("href")
       if (name !== null && name.toLowerCase().trim().includes("cover")) return false;
     }
-   })
+  })
 
   getmeta(encodeURIComponent(name2), cover, folder, acc)
 
 
   list_append();
   song_pos = check_pos(acc);
-  if (document.querySelector('.music_list #list').clientHeight <= 359){
-    $('.music_list #scroll').css('height', document.querySelector('.music_list #list').clientHeight+'px')
-  }else{$('.music_list #scroll').css('height', '360px')};
+  if (document.querySelector('.music_list #list').clientHeight <= 359) {
+    $('.music_list #scroll').css('height', document.querySelector('.music_list #list').clientHeight + 'px')
+  } else { $('.music_list #scroll').css('height', '360px') };
   check3 = false;
   $('.music_list #list li').removeAttr('style');
   $('.music_list #list li').eq(song_pos).css('animation', 'fade 2s ease infinite alternate')
@@ -40,7 +71,9 @@ function play(name2 = '') {
 }
 
 function play_temp(order) {
-  var src = $(elem[order]).attr('href');
+  var src = $(elem[order]).text();
+  console.log(src);
+
   var acc = src.split('.');
   acc.splice(-1);
   acc = acc.join('.');
@@ -49,126 +82,127 @@ function play_temp(order) {
   audio.src = last_dir + src;
   var cover = "";
   getmeta(src, cover, null, acc)
-    
-  song_pos =  check_pos(acc);
+
+  song_pos = check_pos(acc);
   $('.music_list #list li').removeAttr('style');
   $('.music_list #list li').eq(song_pos).css('animation', 'fade 2s ease infinite alternate')
 }
 
-async function getmeta(name, cover, folder, acc){
+async function getmeta(name, cover, folder, acc) {
   var res = await fetch(last_dir + name)
   var blob = await res.blob();
 
   jsmediatags.read(blob, {
-    onSuccess: function(tag) {
+    onSuccess: function (tag) {
       if (tag.tags.title != null) acc = tag.tags.title;
-      if (tag.tags.album != null) {folder = tag.tags.album}
-      else {folder = decodeURI(folder[folder.length-2])};
+      if (tag.tags.album != null) { folder = tag.tags.album }
+      else { folder = decodeURI(folder[folder.length - 2]) };
       if (tag.tags.picture != null) {
         var data = tag.tags.picture.data;
         var format = tag.tags.picture.format;
         let base64String = "";
         for (var i = 0; i < data.length; i++) {
-            base64String += String.fromCharCode(data[i]);
+          base64String += String.fromCharCode(data[i]);
         }
         cover = `data:${data.format};base64,${btoa(base64String)}`;
       }
       $('div.cover img').attr('src', cover)
       document.querySelector('#aud-info').innerText = decodeURIComponent(acc)
-      d.setProperty('--width-text', document.querySelector('#aud-info').offsetWidth/130*110 + '%');
+      d.setProperty('--width-text', document.querySelector('#aud-info').offsetWidth / 130 * 110 + '%');
       $('.music_list span').text(folder);
     },
-    onError: function(error) {
+    onError: function (error) {
       $('div.cover img').attr('src', cover)
       document.querySelector('#aud-info').innerText = decodeURIComponent(acc)
-      d.setProperty('--width-text', document.querySelector('#aud-info').offsetWidth/130*110 + '%');
-      $('.music_list span').text(decodeURI(folder[folder.length-2]));
+      d.setProperty('--width-text', document.querySelector('#aud-info').offsetWidth / 130 * 110 + '%');
+      $('.music_list span').text(decodeURI(folder[folder.length - 2]));
     }
   })
 }
 
-function list_append(){
-    document.querySelector('.music_list #list').innerHTML = ""
+function list_append() {
+  document.querySelector('.music_list #list').innerHTML = ""
 
-    $(elem).each(function() {
-        var name = $(this).attr('href');
-        var acc = name.split('.');
-        acc.splice(-1);
-        acc = acc.join('.');
-        var ext = name.split(".")[name.split(".").length - 1].trim();
-
-        var check = 0
-	    for ( check; check < elem.length; check++) {
-            if ($(elem[check]).attr('href') == name) break;
-	    }
-
-        if (["m4a","mp3","flac","wav","ogg", "aiff"].includes(ext)) {
-            $(".music_list #list").append(`<li><button onclick='play_temp(`+check+`)'>`+decodeURIComponent(acc)+`</button></li>`); 
-        }
-    })
+  $(elem).each(function () {
+    var name = $(this).text();
+    var acc = name.split('.');
+    acc.splice(-1);
+    acc = acc.join('.');
+    var ext = name.split(".")[name.split(".").length - 1].trim();
 
     var check = 0
-    for ( check; check < elem.length; check++) {
-        if ($(elem[check]).attr('href') == $(".fc-media audio").attr('src')) break;
+    for (check; check < elem.length; check++) {
+      if ($(elem[check]).text() == name) break;
     }
+
+    if (["m4a", "mp3", "flac", "wav", "ogg", "aiff"].includes(ext)) {
+      $(".music_list #list").append(`<li><button onclick='play_temp(` + check + `)'>` + decodeURIComponent(acc) + `</button></li>`);
+    }
+  })
+
+  var check = 0
+  for (check; check < elem.length; check++) {
+    if ($(elem[check]).attr('href') == $(".fc-media audio").attr('src')) break;
+  }
 
 }
 
-function check_pos(acc){
-    var check = 0;
-    for ( check; check < $('.music_list #list button').length; check++) {
-        if (decodeURIComponent(acc) == $('.music_list #list button').eq(check).text()) break;
-    }
-    console.log(check);
-    return check;
+function check_pos(acc) {
+  var check = 0;
+  for (check; check < $('.music_list #list button').length; check++) {
+    if (decodeURIComponent(acc) == $('.music_list #list button').eq(check).text()) break;
+  }
+  console.log(check);
+  return check;
 }
 
 
 var check3 = false;
-$('.music_list #hide').click(()=>{
-    check3 = !check3;
-    if (check3 == true){$('.music_list #scroll').css('height', '0');
-    }else{
-        if (document.querySelector('.music_list #list').clientHeight <= 359){
-            $('.music_list #scroll').css('height', document.querySelector('.music_list #list').clientHeight+'px')
-        }else{$('.music_list #scroll').css('height', '360px')}
-    }
+$('.music_list #hide').click(() => {
+  check3 = !check3;
+  if (check3 == true) {
+    $('.music_list #scroll').css('height', '0');
+  } else {
+    if (document.querySelector('.music_list #list').clientHeight <= 359) {
+      $('.music_list #scroll').css('height', document.querySelector('.music_list #list').clientHeight + 'px')
+    } else { $('.music_list #scroll').css('height', '360px') }
+  }
 })
 
-	elem = document.querySelectorAll('.file a');
-	d = document.documentElement.style;
-    $('#menu-panel').parent().before($('#menu-panel'));
+elem = document.querySelectorAll('.file a');
+d = document.documentElement.style;
+$('#menu-panel').parent().before($('#menu-panel'));
 
 
 
-       
+
 
 var cover_check;
 play_pause = false
-$(document).on('keyup',function(e) {
-   if(e.which === 80) {play_pause = !play_pause}
-   if(play_pause == true) {aud.pause()}
-   else {aud.play()}
-   if(e.which === 188) backward();
-   if(e.which === 190) forward();
-   if(e.which === 191) atp();
+$(document).on('keyup', function (e) {
+  if (e.which === 80) { play_pause = !play_pause }
+  if (play_pause == true) { aud.pause() }
+  else { aud.play() }
+  if (e.which === 188) backward();
+  if (e.which === 190) forward();
+  if (e.which === 191) atp();
 })
-$(document).on('keydown',function(e) {
-   if(e.which === 37) aud.currentTime -= 1;
-   if(e.which === 39) aud.currentTime += 1;
+$(document).on('keydown', function (e) {
+  if (e.which === 37) aud.currentTime -= 1;
+  if (e.which === 39) aud.currentTime += 1;
 })
 
 var song_pos = 0;
 var last_dir = "";
-function backward(){
-  if (song_pos > 0){
+function backward() {
+  if (song_pos > 0) {
     song_pos -= 1;
     $('.music_list #list button').eq(song_pos).click();
   }
 }
 
-function forward(){
-  if (song_pos < $('.music_list #list button').length - 1){
+function forward() {
+  if (song_pos < $('.music_list #list button').length - 1) {
     song_pos += 1;
     $('.music_list #list button').eq(song_pos).click();
   }
@@ -176,63 +210,59 @@ function forward(){
 
 var stopint_check = true
 var aud = document.querySelector("audio.fc-media")
-function atp(){
-    stopint_check = !stopint_check;
-	if (stopint_check == true) {
-    aud.onended = function() {
+function atp() {
+  stopint_check = !stopint_check;
+  if (stopint_check == true) {
+    aud.onended = function () {
       console.log('autoplay-off');
     };
-		d.setProperty('--autoplay-color', '#aeaeae4d');	
-	}
-	else{
-		aud.onended = function() {
+    d.setProperty('--autoplay-color', '#aeaeae4d');
+  }
+  else {
+    aud.onended = function () {
       forward();
-    }; 
-		d.setProperty('--autoplay-color', '#c5c9d6');
-	}
+    };
+    d.setProperty('--autoplay-color', '#c5c9d6');
+  }
 }
 $("#auto-play").on("click", atp);
 
-var audio = {    
-    init: function() {        
-    var $that = this;        
-        $(function() {            
-            $that.components.media();        
-        });    
+var audio = {
+  init: function () {
+    var $that = this;
+    $(function () {
+      $that.components.media();
+    });
+  },
+  components: {
+    media: function (target) {
+      var media = $("audio.fc-media", (target !== undefined) ? target : 'body');
+      if (media.length) {
+        media.mediaelementplayer({
+          audioHeight: 100,
+          features: ['playpause', 'current', 'duration', 'progress', 'volume', 'tracks', 'fullscreen', 'next'],
+          alwaysShowControls: true,
+          timeAndDurationSeparator: '<span></span>',
+          iPadUseNativeControls: false,
+          iPhoneUseNativeControls: false,
+          AndroidUseNativeControls: false
+        });
+      }
     },
-    components: {        
-        media: function(target) {            
-            var media = $("audio.fc-media", (target !== undefined) ? target : 'body');            
-            if (media.length) {     
-                media.mediaelementplayer({                    
-                    audioHeight: 100,
-                    features : ['playpause', 'current', 'duration', 'progress', 'volume', 'tracks', 'fullscreen', 'next'],
-                    alwaysShowControls      : true,
-                    timeAndDurationSeparator: '<span></span>',
-                    iPadUseNativeControls: false,
-                    iPhoneUseNativeControls: false,
-                    AndroidUseNativeControls: false                
-                });            
-            }        
-        },
-            
-    },
+
+  },
 };
-audio.init();
+
 
 //make thing dragable
-dragElement(document.querySelector(".music-player"));
-dragElement(document.querySelector(".music_list"));
+
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.querySelector(elmnt.id + "header")) {
-    /* if present, the header is where you move the DIV from:*/
-    document.querySelector(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    elmnt.onmousedown = dragMouseDown;
-  }
+
+  /* otherwise, move the DIV from anywhere inside the DIV:*/
+  elmnt.onmousedown = dragMouseDown;
+
 
   function dragMouseDown(e) {
     e = e || window.event;
@@ -264,3 +294,12 @@ function dragElement(elmnt) {
     document.onmousemove = null;
   }
 }
+
+setTimeout(() => {
+  dragElement(document.querySelector(".music-player"))
+  dragElement(document.querySelector(".music_list"))
+  audio.init()
+}, 2000)
+// document.addEventListener("load", () => {
+
+// })
