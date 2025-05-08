0a8e011ffa801c49b6dabfc0ebcd97f4c5109885
ahrixthinh <111081318+thinhthui@users.noreply.github.com>
2025-05-08 19:14:34 +0700

    Update plugin.js

--- dist/plugin.js
+++ dist/plugin.js
@@ -3,11 +3,11 @@ exports.description = "Not simple player"
 exports.repo = "thinhthui/not-simple-player"
 exports.apiRequired = 8.1
 exports.frontend_js = [
-    'main.js',
     'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/mediaelement/4.2.7/mediaelement-and-player.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/jsmediatags/3.9.5/jsmediatags.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.0/FileSaver.min.js',
+    'main.js',
 ]
 exports.frontend_css = [
     'style.css',
